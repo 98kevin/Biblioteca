f9076c10377fbe69447019b45f8ace2ede3f1f4a
LGuilln <lueduardogr@hotmail.com>
2019-07-27 02:53:26 -0600

    Actualizacion agregando Metodos

--- src/main/java/com/backend/Main.java
+++ src/main/java/com/backend/Main.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.kevin;
-
+package com.backend;
 import javax.swing.JOptionPane;
 
 /**
--- src/main/java/com/backend/Prestamo.java
+++ src/main/java/com/backend/Prestamo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.kevin;
+package com.backend;
 
 import java.io.Serializable;
 import java.sql.Date;
